75e5d386331628e218e3bc42de6d59b627f2375a
tak3r <indrakurniawan86@yahoo.com>
2013-06-16 21:17:38 +1000

    -NEW: added Notebook project into the repository. -NEW: added global git ignore file

---
+++ wpf/Notebook/Notebook/Model/Income.cs
@@ -0,0 +1,27 @@
+﻿namespace Notebook.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    internal class Income
+    {
+        public DateTime Date {get;set;}
+
+        public string Buyer { get; set; }
+
+        public string Address { get; set; }
+
+        public string NPWP { get; set; }
+
+        public List<Product> Items { get; private set; }
+
+        string Note { get; set; }
+
+        public Income()
+        {
+            this.Items = new List<Product>();
+        }
+    }
+}
