6c57364b2a0fbd54841706b691d3131a6e06f355
unknown <gsadikin@gmail.com>
2013-06-26 22:55:15 +1000

    -CHANGE: storeAnItem function added

--- wpf/Notebook/Notebook/Model/Income.cs
+++ wpf/Notebook/Notebook/Model/Income.cs
@@ -5,6 +5,8 @@
     using System.Linq;
     using System.Text;
     using SQLDataAccessLayer;
+    using System.Configuration;
+    using System.Data.Common;
 
     internal class Income
     {
@@ -21,6 +23,7 @@
         string Note { get; set; }
 
         public string Invoice_number { get; set; }
+        private SqlManager sqlManager { get; set; }
 
         public Income(string invoice_number, string buyer, string address, DateTime date)
         {
@@ -29,6 +32,7 @@
             this.Buyer = buyer;
             this.Address = address;
             this.Items = new List<Product>();
+            sqlManager = GetDBConnection();
         }
 
         public Income(DateTime date)
@@ -42,7 +46,7 @@
             item.Name  = name;
             item.Price = price;
             item.Quantity = quantity;
-            this.Items.Add(item);
+            Items.Add(item);
         }
 
         public void Load()
@@ -50,22 +54,46 @@
             // TODO: load the data from database.
             // function call to db to load stuff
             // LoadDataFromDb(this.Date)
+            object[] result = sqlManager.SelectFrom(
+                   "income",
+                   new string[] { "invoice_number", "date", "buyer", "address", "item", "price", "quantity" },
+                   string.Empty);
+
+            foreach (object record in result)
+            {
+
+              
+            }
         }
 
         public void Save()
         {
-             SQLConnectionFactory sqlConnectionFactory = new SQLConnectionFactory();
-             IDbConnection connection
-            // TODO: Store the data into database.
-            foreach (Product item in this.Items)
+            // Store the data into database.
+            foreach (Product item in Items)
             {
                 // function call to db to store each item
                 // SaveDataToDb(this.Invoice_number, this.Date, this.Buyer, this.Address, item.Name, item.Price, item.Quantity)
-
+                sqlManager.InsertInto(
+                        "income",
+                        new string[] { "invoice_number", "date","buyer", "address", "item","price", "quantity" },
+                        new object[] { Invoice_number, Date, Buyer, Address, item.Name, item.Price, item.Quantity},
+                        string.Empty);
             }
         }
 
-        public 
+        public SqlManager GetDBConnection()
+        {
+            string dataProvider = ConfigurationManager.AppSettings["dataProvider"];
+            string connectionString = ConfigurationManager.ConnectionStrings[dataProvider].ConnectionString;
+            SQLConnectionFactory sqlConnection = new SQLConnectionFactory(DbProviderFactories.GetFactory(dataProvider), connectionString);
+            sqlConnection.Connection.Open();
+            SqlManager sqlManager = new SqlManager(sqlConnection.Connection);
+            //if (sqlConnection.Connection.State == System.Data.ConnectionState.Open)
+            return sqlManager;
+            
+           
+
+        }
 
     }
 }
